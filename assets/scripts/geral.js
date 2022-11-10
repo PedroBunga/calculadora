6a039791830c4fe83cfa587db7e84129e1dca65a
Pedro Bunga <117820477+PedroBunga@users.noreply.github.com>
2022-11-10 17:28:15 +0100

    Pedro Bangui Bunga
    Ate ao momento ainda não criei todoas as funcionalidades, como é o caso das operações artmeticas, do tema Dark entre outras coisas. tambem ainda não foi feito a parte responsiva do site,mas nas proximas ocasiões serão feitas

--- assets/scripts/geral.js
+++ assets/scripts/geral.js
@@ -1,11 +1,14 @@
-
 function setResultValue() {
   if (document.getElementById("entrada").value != null) {
-       document.getElementById("result").innerHTML=document.getElementById("entrada").value;
+    document.getElementById("result").innerHTML =
+      document.getElementById("entrada").value;
+  }else{
+    document.getElementById("entrada").value="Insere Um Valor Valido!"
   }
 }
 
-
-function clearScrean(){
-    document.getElementById("result").innerHTML=document.getElementById("entrada").value="";
+function clearScrean() {
+  document.getElementById("result").innerHTML = document.getElementById(
+    "entrada"
+  ).value = "";
 }
