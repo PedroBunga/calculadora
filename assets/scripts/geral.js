d8d5ef0ad1227e03b522104e9466740a2cef760b
Pedro Bunga <117820477+PedroBunga@users.noreply.github.com>
2022-11-17 09:41:54 +0100

    Pedro Bangui Bunga
    Tornado o codigo mais limpo.

--- assets/scripts/geral.js
+++ assets/scripts/geral.js
@@ -1,26 +1,16 @@
-
 /*Estas Funções Servem para Capturar o Valor Presionado na Calculadora e concatenar ao Input existente 
   Isto se aplica tambem as operações*/
-function getTecla (valor_da_tecla){
-  let tamanho_da_entrada =document.getElementById("entrada").value;
-  if (tamanho_da_entrada.length===31) {
-    return ;
+function getTecla(valor_da_tecla) {
+  let tamanho_da_entrada = document.getElementById("entrada").value;
+  if (tamanho_da_entrada.length === 31) {
+    return;
   }
   return (document.getElementById("entrada").value += `${valor_da_tecla}`);
 }
 /*Capturando as operações escolhidas*/
-function clicou_sum() {
-  return (document.getElementById("entrada").value += "+");
-}
 
-function clicou_sub() {
-  return (document.getElementById("entrada").value += "-");
-}
-function clicou_mult() {
-  return (document.getElementById("entrada").value += "*");
-}
-function clicou_div() {
-  return (document.getElementById("entrada").value += "/");
+function getOperation(operation) {
+  return (document.getElementById("entrada").value += `${operation}`);
 }
 
 /*Estas funções Servem para capturar a operação escolhida e concatenar ao input existente*/
@@ -29,25 +19,15 @@ function resetInputValue() {
   document.getElementById("entrada").placeholder = "Insere a Operação";
   document.getElementById("entrada").value = "";
 }
-
+/* Esta Função verifica na string passada "input" qual é a operação que o usuario escolheu */
 function whatOperation(expressao) {
   let operatio = "";
-
   for (let i = 0; i < expressao.length; i++) {
-    if (expressao[i] === "-") {
-      operatio = "-";
-    }
-    if (expressao[i] === "+") {
-      operatio = "+";
-    }
-    if (expressao[i] === "*") {
-      operatio = "*";
-    }
-    if (expressao[i] === "/") {
-      operatio = "/";
-    }
+    if (expressao[i] === "-") {operatio = "-";}
+    if (expressao[i] === "+") {operatio = "+";}
+    if (expressao[i] === "*") {operatio = "*";}
+    if (expressao[i] === "/") {operatio = "/";}
   }
-
   return operatio;
 }
 
@@ -55,31 +35,37 @@ function whatOperation(expressao) {
 function resultado() {
   var resultado_Final = 0;
   let valo_Digitado = document.getElementById("entrada").value;
-  let operacao =whatOperation(valo_Digitado);
+  let operacao = whatOperation(valo_Digitado);
   let valorAtual = valo_Digitado.split(`${operacao}`);
 
   if (operacao === "+") {
     resultado_Final = parseInt(valorAtual[0], 10) + parseInt(valorAtual[1], 10);
-  }else{
+  } else {
     if (operacao === "*") {
-      resultado_Final = parseInt(valorAtual[0], 10)*parseInt(valorAtual[1], 10);
+      resultado_Final =
+        parseInt(valorAtual[0], 10) * parseInt(valorAtual[1], 10);
     } else {
-      if (operacao === "/" && valorAtual[1]>0 ) {
-        resultado_Final = parseFloat(parseInt(valorAtual[0], 10)/parseInt(valorAtual[1], 10));
+      if (operacao === "/" && valorAtual[1] > 0) {
+        resultado_Final = parseFloat(
+          parseInt(valorAtual[0], 10) / parseInt(valorAtual[1], 10)
+        );
       } else {
-        if (operacao === "-" && valorAtual[0]|valorAtual[1]>0 ) {
-          resultado_Final = parseFloat(parseInt(valorAtual[0], 10)-parseInt(valorAtual[1], 10));
-        } else {
-          
+        if (operacao === "-" && valorAtual[0] | (valorAtual[1] > 0)) {
+          resultado_Final = parseFloat(
+            parseInt(valorAtual[0], 10) - parseInt(valorAtual[1], 10)
+          );
         }
       }
     }
   }
-  
 
-  if (document.getElementById("entrada").value ==="" || document.getElementById("entrada").value ===null) {
-    return (document.getElementById("entrada").placeholder = "Entrada Invalida!");
+  if (
+    document.getElementById("entrada").value === "" ||
+    document.getElementById("entrada").value === null
+  ) {
+    return (document.getElementById("entrada").placeholder =
+      "Entrada Invalida!");
   }
 
-  return document.getElementById("result").innerHTML = resultado_Final;
+  return (document.getElementById("result").innerHTML = resultado_Final);
 }
